bb1f5e86f6b167095dbc1ca8c9420418639f9d36
Eugen Pushkaroff <eugenpushkaroff@gmail.com>
2015-02-20 16:50:12 +0200

    FrontendArchitectureGrunt

---
+++ src/main/webapp/grunt/concat.js
@@ -0,0 +1,20 @@
+module.exports = {
+    components:{
+        src:[
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/slimScroll/jquery.slimscroll.js',
+            'bower_components/moment/moment.js',
+            'bower_components/bootstrap/dist/js/bootstrap.js',
+            'bower_components/bootstrap-fileinput/js/fileinput.js',
+            'bower_components/datatables/media/js/jquery.dataTables.js',
+            'bower_components/datatables-bootstrap3/BS3/assets/js/datatables.js',
+            'bower_components/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js',
+            'bower_components/bootstrap-multiselect/dist/js/bootstrap-multiselect.js',
+            'bower_components/bootstrap-switch/dist/js/bootstrap-switch.js',
+            'bower_components/bootstrap-select/js/bootstrap-select.js',
+            'bower_components/bootstrap-validator/js/validator.js',
+            'resources/js/scripts.js'
+        ],
+        dest:'resources/js/compiled.js'
+    }
+}
---
+++ src/main/webapp/grunt/uglify.js
@@ -0,0 +1,8 @@
+module.exports = {
+    components: {
+        src:[
+            'resources/js/compiled.js'
+        ],
+        dest:'resources/js/compiled.min.js'
+    }
+}
