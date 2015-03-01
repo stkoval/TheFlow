45b1ebea1150906c2a0aef6be62daccaf0e71620
ksep <skoval.mail@gmail.com>
2015-03-01 10:57:56 +0200

    smallfix

--- src/main/java/com/theflow/configuration/SecurityConfig.java
+++ src/main/java/com/theflow/configuration/SecurityConfig.java
@@ -54,7 +54,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .rememberMe()
                 .and()
-                .csrf().disable()
                 .sessionManagement()
                 .maximumSessions(1);
     }
